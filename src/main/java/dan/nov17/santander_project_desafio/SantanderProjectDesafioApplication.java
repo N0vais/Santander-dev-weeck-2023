0e8372b4b1acc773463b9a6af20c542ef8a6a27c
Daniel Novais <danlicesol@gmail.com>
2023-10-22 18:18:46 -0300

    criaçao do projeto

---
+++ src/main/java/dan/nov17/santander_project_desafio/SantanderProjectDesafioApplication.java
@@ -0,0 +1,13 @@
+package dan.nov17.santander_project_desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderProjectDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderProjectDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dan/nov17/santander_project_desafio/SantanderProjectDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package dan.nov17.santander_project_desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderProjectDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
