fe583721c9322faae6558e4ebc0f32e57e582ddc
Daniel Novais <danlicesol@gmail.com>
2023-10-22 21:29:26 -0300

    realizando alguns ajustes

--- src/main/java/dan/nov17/santander_project_desafio/SantanderProjectDesafioApplication.java
+++ src/main/java/dan/nov17/santander_project_desafio/SantanderProjectDesafioApplication.java
@@ -3,6 +3,8 @@ package dan.nov17.santander_project_desafio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//para corrigir o problema de cors acrecentando a @openApidefinition
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class SantanderProjectDesafioApplication {
 
